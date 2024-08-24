70cb8a29e963e76b25b7cf005e7dbe08ef8fd29e
大动物 <15294745236@163.com>
2024-08-24 20:10:26 +0800

    :sparkles:添加小窗口

--- src/content/content.ts
+++ src/content/content.ts
@@ -10,9 +10,33 @@ const style = `
 `
 let preEl: Element | null = null
 
+console.log('content file')
+const init = () => {
+  const addIframe = (id: string, pagePath: string) => {
+    const contentIframe = document.createElement("iframe");
+    contentIframe.id = id;
+    contentIframe.style.cssText = `width: 300px;
+                                   height: 500px;
+                                   position: fixed;
+                                   z-index: 10000002; 
+                                   border: none;
+                                   top: 10px;
+                                   left: 10px;`;
+    const getContentPage = chrome.runtime.getURL(pagePath);
+    contentIframe.src = getContentPage;
+    document.body.appendChild(contentIframe);
+  }
 
+  addIframe('content-start-iframe', 'contentPage/index.html')
+}
+
+// 判断 window.top 和 self 是否相等，如果不相等，则不注入 iframe
+// if (window.top === window.self) {
+//     init();
+//   }
 const handleEvent = (event: MouseEvent) => {
     let cur = document.elementFromPoint(event.clientX, event.clientY)!
+    if(cur.tagName === 'IFRAME')return 
     if (cur !== preEl) {
         preEl && preEl.classList.remove(timeS)
         preEl = cur
@@ -24,7 +48,7 @@ const handleEvent = (event: MouseEvent) => {
 const MeasureTools = (e: string) => {
 
     if (e = eventTypes.open) {
-        console.log(e);
+        init()
         window.addEventListener('mousemove', handleEvent)
     } else if (e = eventTypes.close) {
         window.removeEventListener('mousemove', handleEvent)
--- src/contentPage/App.tsx
+++ src/contentPage/App.tsx
@@ -1,4 +1,14 @@
 import './App.css'
+import { DragOutlined } from '@ant-design/icons';
 export default function App() {
-  return <div id="app">Hello, World!!</div>;
+    const drag = () => {
+            
+    }
+    return <>
+        <div>
+            <div id='drag-icon'>   <DragOutlined onClick={drag} style={{ fontSize: '24px', color: 'rgb(53, 150, 235)', cursor: "grab" }} /></div>
+            <div id="app">当前元素信息</div>
+        </div>
+
+    </>;
 }
