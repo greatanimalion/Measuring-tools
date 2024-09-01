93c1472ff40c5669880fbd77a209cd4312241cea
大动物 <15294745236@163.com>
2024-09-01 20:29:26 +0800

    优化样式

--- src/contentPage/components/ElemenetMessage.tsx
+++ src/contentPage/components/ElemenetMessage.tsx
@@ -1,27 +1,24 @@
 // import { Button, Input } from "@mui/material";
 
-import { Fab } from "@mui/material";
+// import { Fab } from "@mui/material";
 import { useEffect, useState } from "react";
 
 export const ElementMessage = () => {
   const [obj, setObj] = useState();
   useEffect(() => {
     window.addEventListener('message', (e) => {
-      console.log(e.data);
       setObj(e.data)
     }, false)
   })
-
   let e=obj?Object.keys(obj!):[];
   let i=e.map((item,index)=>{
-    return <p key={index}><strong style={{ color: 'blue' }}>{item}:</strong>{obj![item]}</p>
+    if(index==0)return <p key={index}  style={{"fontFamily": "cursive","marginBottom":"10px"}}>
+      <strong style={{ color: '#616161' }}>{item}:</strong>{obj![item]}</p>
+    return <p key={index}  style={{"fontFamily": "cursive","backgroundColor":" #ececec","padding":"10px","margin":"5px 0","borderRadius": "5px"}}>
+      <strong style={{ color: '#616161' }}>{item}:</strong>{obj![item]}</p>
   })
   return <>
     <div style={{ marginTop: '48px' }}>
-      <Fab color="primary" aria-label="add" style={{ position: "fixed", right: 10, height: 47, width: 47 }}>
-        复制
-      </Fab>
-     
       {i}
 
     </div>
